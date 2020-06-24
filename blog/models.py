5dc0d53cbe62e14bbb2e6e10178880a03cdd9cf5
ENIGMAxDEV <enigmaxdev@gmail.com>
2020-06-24 21:13:58 +0300

    finished of make posts and show it in page

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    post_date = models.DateTimeField(default=timezone.now)
+    post_update = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title

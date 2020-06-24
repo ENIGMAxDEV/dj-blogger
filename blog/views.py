e1175469cffc3743e9f9126fa7240e0f62f50a42
ENIGMAxDEV <enigmaxdev@gmail.com>
2020-06-24 11:34:00 +0300

    finished added home page and base page

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'title': 'عنوان التدوينة',
+        'content': 'محتوى التدوينة',
+        'date': '2020-1-1',
+        'author': 'المؤلف الاول',
+    },
+    {
+        'title': 'عنوان التدوينة',
+        'content': 'محتوى التدوينة',
+        'date': '2020-1-1',
+        'author': 'المؤلف الاول',
+    },
+]
+
 
 def home(r):
-    return render(r, 'index.html', {})
+    context = {
+        'title': 'الصفحة الرئيسية',
+        'posts': posts,
+    }
+    return render(r, 'index.html', context)
+
+
+def about(r):
+    context = {
+        'title': 'من أنا',
+    }
+    return render(r, 'about.html', context)
